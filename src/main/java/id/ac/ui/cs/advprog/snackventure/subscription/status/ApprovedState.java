0d055170e341539e82aad35ccead98d78ed600c0
Fahmi Ramadhan <51454564+fahmi-ramadhan@users.noreply.github.com>
2024-04-24 18:25:45 +0700

    [GREEN] Set endDate when the approved subscription is cancelled

--- src/main/java/id/ac/ui/cs/advprog/snackventure/subscription/status/ApprovedState.java
+++ src/main/java/id/ac/ui/cs/advprog/snackventure/subscription/status/ApprovedState.java
@@ -2,6 +2,7 @@ package id.ac.ui.cs.advprog.snackventure.subscription.status;
 
 import id.ac.ui.cs.advprog.snackventure.subscription.enums.*;
 import id.ac.ui.cs.advprog.snackventure.subscription.model.Subscription;
+import java.util.Date;
 
 public class ApprovedState implements SubscriptionState {
     @Override
@@ -17,6 +18,7 @@ public class ApprovedState implements SubscriptionState {
     @Override
     public void cancel(Subscription subscription) {
         subscription.setSubscriptionStatus(SubscriptionStatus.CANCELLED);
+        subscription.setEndDate(new Date());
         subscription.setState(new CancelledState());
     }
 }
