9e415308512987ff7726aad97b757557c8f8715c
Fahmi Ramadhan <51454564+fahmi-ramadhan@users.noreply.github.com>
2024-05-01 19:42:30 +0700

    feat: add endpoints in Subscription Controller

--- src/main/java/id/ac/ui/cs/advprog/snackventure/subscription/controller/SubscriptionController.java
+++ src/main/java/id/ac/ui/cs/advprog/snackventure/subscription/controller/SubscriptionController.java
@@ -1,13 +1,50 @@
 package id.ac.ui.cs.advprog.snackventure.subscription.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import id.ac.ui.cs.advprog.snackventure.subscription.model.Subscription;
+import id.ac.ui.cs.advprog.snackventure.subscription.service.SubscriptionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@RestController()
+import java.util.List;
+import java.util.Map;
+
+@RestController
 public class SubscriptionController {
-    @GetMapping("/")
-    public String subscription() {
-        return "Hello from subscription!";
+
+    @Autowired
+    private SubscriptionService subscriptionService;
+
+    @PostMapping("/subscription-box/{id}/subscribe")
+    public ResponseEntity<Subscription> createSubscription(@PathVariable String id, @RequestBody Map<String, String> requestBody) {
+        String frequency = requestBody.get("frequency");
+        String customerId = requestBody.get("customerId");
+        Subscription createdSubscription = subscriptionService.createSubscription(frequency, id, customerId);
+        return ResponseEntity.ok(createdSubscription);
+    }
+
+    @GetMapping("/subscriptions")
+    public ResponseEntity<List<Subscription>> getAllSubscriptions() {
+        List<Subscription> subscriptions = subscriptionService.getAllSubscriptions();
+        return ResponseEntity.ok(subscriptions);
+    }
+
+    @GetMapping("subscriptions/{id}")
+    public ResponseEntity<Subscription> getSubscriptionById(@PathVariable String id) {
+        Subscription subscription = subscriptionService.findById(id);
+        return ResponseEntity.ok(subscription);
+    }
+
+    @PutMapping("subscriptions/{id}/change-status")
+    public ResponseEntity<Subscription> updateApprovalStatus(@PathVariable String id, @RequestBody Map<String, String> requestBody) {
+        String status = requestBody.get("status");
+        Subscription updatedSubscription = subscriptionService.updateApprovalStatus(id, status);
+        return ResponseEntity.ok(updatedSubscription);
+    }
+
+    @PutMapping("subscriptions/{id}/unsubscribe")
+    public ResponseEntity<Subscription> cancelSubscription(@PathVariable String id) {
+        Subscription cancelledSubscription = subscriptionService.cancelSubscription(id);
+        return ResponseEntity.ok(cancelledSubscription);
     }
 }
