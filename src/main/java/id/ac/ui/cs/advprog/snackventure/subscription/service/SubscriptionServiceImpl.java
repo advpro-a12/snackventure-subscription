942df487748f13e00461180aae53df5b95274bee
Fahmi Ramadhan <51454564+fahmi-ramadhan@users.noreply.github.com>
2024-05-01 14:10:53 +0700

    [GREEN] implement more methods in Subscription Service

--- src/main/java/id/ac/ui/cs/advprog/snackventure/subscription/service/SubscriptionServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/snackventure/subscription/service/SubscriptionServiceImpl.java
@@ -1,11 +1,13 @@
 package id.ac.ui.cs.advprog.snackventure.subscription.service;
 
+import id.ac.ui.cs.advprog.snackventure.subscription.enums.ApprovalStatus;
 import id.ac.ui.cs.advprog.snackventure.subscription.model.Subscription;
 import id.ac.ui.cs.advprog.snackventure.subscription.repository.SubscriptionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class SubscriptionServiceImpl implements SubscriptionService {
@@ -25,16 +27,32 @@ public class SubscriptionServiceImpl implements SubscriptionService {
 
     @Override
     public Subscription findById(String subscriptionId) {
-        return null;
+        UUID id = UUID.fromString(subscriptionId);
+        return subscriptionRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid subscription Id:" + subscriptionId));
     }
 
     @Override
     public Subscription updateApprovalStatus(String subscriptionId, String status) {
-        return null;
+        UUID id = UUID.fromString(subscriptionId);
+        Subscription subscription = this.findById(String.valueOf(id));
+        if (status.equalsIgnoreCase(ApprovalStatus.APPROVED.toString())) {
+            subscription.approve();
+        } else if (status.equalsIgnoreCase(ApprovalStatus.REJECTED.toString())) {
+            subscription.reject();
+        } else {
+            throw new IllegalArgumentException("Invalid status");
+        }
+        subscriptionRepository.save(subscription);
+        return subscription;
     }
 
     @Override
-    public Subscription cancelSubscription(String subscriptionId, String status) {
-        return null;
+    public Subscription cancelSubscription(String subscriptionId) {
+        UUID id = UUID.fromString(subscriptionId);
+        Subscription subscription = this.findById(String.valueOf(id));
+        subscription.cancel();
+        subscriptionRepository.save(subscription);
+        return subscription;
     }
 }
